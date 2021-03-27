515e1e1a59d3b452fcad90de3dd0cca4247308e4
Serhii Hayvoronskyi <hayvoronskyi@ukr.net>
2021-03-27 02:15:44 +0200

    fix readme

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+const API_KEY = '19173841-1948c6c8698f2c876b72e0f5c';
+const BASE_URL = 'https://pixabay.com/api';
+
 const api = async (query, page) => {
   const { data } = await axios.get(
-    `https://pixabay.com/api/?q=${query}&page=${page}&key=18963132-a5d8582da162d4c0f5ee62384&image_type=photo&orientation=horizontal&per_page=12`,
+    `${BASE_URL}/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
   );
   return data.hits;
 };
